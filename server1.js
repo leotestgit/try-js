9ae2df5cdfa2a2bab267a47cf4541c6f89e7e5ae
leotestgit <93026813+leotestgit@users.noreply.github.com>
2021-10-24 09:07:04 +0530

    Update server1.js

--- server1.js
+++ server1.js
@@ -5,8 +5,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Cesar la pachu');
 });
+<button onclick="window.close();">Cerrar esta ventana" </button> 
 
 server.listen(port, () => {
   console.log(`Server running on port: ${port}`);
