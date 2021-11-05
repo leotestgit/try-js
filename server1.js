823d99064c6ef72c7becc6d90b0055dc05f3285e
leotestgit <93026813+leotestgit@users.noreply.github.com>
2021-11-05 06:49:24 +0530

    Update server1.js

--- server1.js
+++ server1.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Cesar la pachu');
+  res.end('Hello world');
 });
 <button onclick="window.close();">Cerrar esta ventana" </button> 
 
