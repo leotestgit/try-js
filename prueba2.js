b9506126616883a888c31ef1d0a004756fef4afd
leotestgit <93026813+leotestgit@users.noreply.github.com>
2021-11-05 06:51:25 +0530

    Create prueba2.js

---
+++ prueba2.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const port = 22;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Cesar la pachu');
+});
+<button onclick="window.close();">Cerrar esta ventana" </button> 
+
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
